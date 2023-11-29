df39cb528097fd906b4f4ad432320d6d024dbc9a
Stefan Smudja <stefan.smudja@consid.se>
2023-11-29 15:46:53 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lilstiffy.mockgps"
         minSdk = 28
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

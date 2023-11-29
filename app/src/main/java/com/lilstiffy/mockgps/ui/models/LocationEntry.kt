494c0c8a536ce424b047111bd4a1c740b0fbf932
Stefan Smudja <stefan.smudja@consid.se>
2023-11-29 15:46:17 +0100

    fix proguard

--- app/src/main/java/com/lilstiffy/mockgps/ui/models/LocationEntry.kt
+++ app/src/main/java/com/lilstiffy/mockgps/ui/models/LocationEntry.kt
@@ -1,9 +1,7 @@
 package com.lilstiffy.mockgps.ui.models
 
-import androidx.annotation.Keep
 import com.google.android.gms.maps.model.LatLng
 
-@Keep
 data class LocationEntry(
     var latLng: LatLng,
     var addressLine: String? = null
